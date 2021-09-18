8c0eda1156cab4264f2054d4fe56fe2af9ce4a9f
Hardik Vij <62573477+hardikvij195@users.noreply.github.com>
2021-09-18 21:36:33 +0530

    1

--- main.py
+++ main.py
@@ -4,21 +4,7 @@ from firebase_admin import credentials, firestore, initialize_app
 from google.cloud import storage
 from NewModelIO import PredCluster
 import pandas as pd
-
-
-# import joblib
-
-# from FirebaseIO import MCFunc
-# import firebase
-# from ModelIO import PredCluster
-
-# from google.cloud.storage import app_identity
-# import pandas as pd
-
-# import logging
-# import cloudstorage as gcs
-# import webapp2
-# from google.appengine.api import app_identity
+import jsonpickle
 
 app = Flask(__name__)
 
@@ -30,11 +16,11 @@ db = firestore.client()
 @app.route('/add', methods=['POST'])
 def create():
     try:
-        # BUCKET_NAME = 'testmodelrepo'
-        # PROJECT_ID = 'testingprojects-b6504'
+        BUCKET_NAME = 'testmodelrepo'
+        PROJECT_ID = 'testingprojects-b6504'
 
-        BUCKET_NAME = 'testx_model_repo'
-        PROJECT_ID = 'appsyntests-322514'
+        # BUCKET_NAME = 'testx_model_repo'
+        # PROJECT_ID = 'appsyntests-322514'
         GCS_MODEL = 'API_mini_model.joblib'
 
         client = storage.Client(PROJECT_ID)
@@ -55,8 +41,6 @@ def create():
         # .where('Gender', '!=', gender)
         all_users = [doc.to_dict() for doc in all_users.stream()]
 
-        # model = joblib.load(r"/tmp/API_mini_model.joblib")
-
         df = pd.DataFrame.from_dict(all_users)
         # df = df.drop('Name')
         df.set_index("Name", inplace=True)
@@ -76,13 +60,69 @@ def create():
 @app.route('/list', methods=['POST'])
 def list():
     try:
+        id = request.json['Id']
         gender = request.json['Gender']
-        all_users = db.collection('root').where('Gender', '!=', gender)
-        all_users = [doc.to_dict() for doc in all_users.stream()]
+        all_users = db.collection('root').where(
+            'Gender', '!=', gender).stream()
+        user_list = []
+        #docs = db.collection(u'cities').where(u'capital', u'==', True).stream()
+        # all_users = [doc.to_dict() for doc in all_users.stream()]
+        for doc in all_users:
+            #print(f'{doc.id} => {doc.to_dict()}')
+            d = doc.to_dict()
+            user_list.append(
+                users(doc.id, d['Name'], d['Gender'], d['Smoking'], d['Drinking']))
 
-        return jsonify(all_users), 200
+        upload_recomm_users = db.collection('root').document(
+            id).collection('DateRecommended')
+
+        #user_list2 = jsonpickle.encode(user_list, unpicklable=False)
+        for index, dc in enumerate(user_list):
+            print("index " + str(index) + ": " + str(dc.Id))
+            data = {
+                u'n': dc.Name,
+                u's': dc.Smoking,
+                u'd': dc.Drinking,
+                u'g': dc.Gender,
+                u'o': index
+            }
+            upload_recomm_users.document(dc.Id).set(data)
+        return jsonpickle.encode(user_list, unpicklable=False), 200
     except Exception as e:
         return f"An Error Occured: {e}"
+
+
+class users:
+    def __init__(self, Id, Name, Gender, Smoking, Drinking):
+        self.Id = Id
+        self.Name = Name
+        self.Gender = Gender
+        self.Smoking = Smoking
+        self.Drinking = Drinking
+
+
+@app.route('/')
+def hello():
+    return 'Hello Hardik!'
+
+
+if __name__ == '__main__':
+    app.run(threaded=True, host='127.0.0.1', port=8080, debug=True)
+
+
+# Python3 code here creating class
+
+
+# # creating list
+# list = []
+
+# # appending instances to list
+# list.append(users(1, 'User 1', 'Male', 'Smoking', 'Drinking'))
+# list.append(users(2, 'User 2', 'Female', 'Non-Smoking', 'Non-Drinking'))
+# list.append(users(3, 'User 3', 'Male', 'Non-Smoking', 'Non-Drinking'))
+
+# for obj in list:
+#     print(obj.id, obj.name, sep=' ')
 
 
 # @app.route('/list', methods=['GET'])
@@ -162,29 +202,3 @@ def list():
 
 
 # port = int(os.environ.get('PORT', 8080))
-
-@app.route('/')
-def hello():
-    return 'Hello Hardik!'
-
-
-# @app.route("/api/sum/")
-# def sum():
-#     return "Get Sum!"
-
-
-# @app.route("/api/getno/<int:n>")
-# def getnum(n):
-#     res = {
-#         "num": n
-#     }
-#     return res
-
-
-# @app.route("/api/name/<name>")
-# def name(name):
-#     return name
-
-
-if __name__ == '__main__':
-    app.run(threaded=True, host='127.0.0.1', port=8080, debug=True)
